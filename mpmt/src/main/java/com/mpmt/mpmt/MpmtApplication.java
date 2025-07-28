cbf6a8997dcfae986c2a27fddbeff5e5831f6503
Jouannon Nathan <nathan.jouannon@gmail.com>
2025-07-28 22:55:23 +0200

    initialize spring boot server

---
+++ mpmt/src/main/java/com/mpmt/mpmt/MpmtApplication.java
@@ -0,0 +1,13 @@
+package com.mpmt.mpmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpmtApplication.class, args);
+	}
+
+}
---
+++ mpmt/src/test/java/com/mpmt/mpmt/MpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpmt.mpmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
