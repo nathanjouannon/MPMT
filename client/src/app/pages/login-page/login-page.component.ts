5b8911c6658e7dbd07785bdd9acf0c16d9e41a4a
Jouannon Nathan <nathan.jouannon@gmail.com>
2025-08-17 11:43:18 +0200

    add connected user variables in localstorage

--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -27,7 +27,10 @@ export class LoginPageComponent {
     this.http.post<any>('http://localhost:8080/api/auth/login', data).subscribe({
       next: (response) => {
         localStorage.setItem('token', response.token);
-        this.router.navigate(['/dashboard']); // à créer plus tard
+        localStorage.setItem('current_userID', response.id)
+        localStorage.setItem('current_userName', response.username)
+        localStorage.setItem('current_userEmail', response.email)
+        this.router.navigate(['/dashboard']);
       },
       error: (err) => {
         this.error = err.error.message || 'Email ou mot de passe incorrect';
